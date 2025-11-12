94e6cd906ee6738a720a377d4b9048a780675698
DenGolivets <141760767+DenGolivets@users.noreply.github.com>
2025-11-12 16:25:00 +0200

    change og-image url

---
+++ src/components/meta/OpenGraphTags.tsx
@@ -0,0 +1,12 @@
+export default function OpenGraphTags() {
+  return (
+    <>
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:site_name" content="Esviem Defence" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="https://www.esviem-defence.com/logo/esviem_defence_logo_2_2.png" />
+    </>
+  );
+}
